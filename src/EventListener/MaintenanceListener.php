4e8cbfedac238f18903f3b73f1b1ab48523314af
Manon Lavenier <m.lavenier@monsieurbiz.com>
2021-09-27 15:22:26 +0200

    On request show the maintenance page

--- src/EventListener/MaintenanceListener.php
+++ src/EventListener/MaintenanceListener.php
@@ -1,13 +1,12 @@
 <?php
-namespace App\Controller\EventListener;
+namespace App\EventListener;
 
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\RequestEvent;
-
+use Symfony\Component\HttpKernel\Event\RequestEvent;
 
 class MaintenanceListener
 {
-    public function onKernelRequest(requestEvent $event){
+    public function onKernelRequest(RequestEvent $event){
         $event->setResponse(
             new Response('En maintenance',
             Response::HTTP_SERVICE_UNAVAILABLE
