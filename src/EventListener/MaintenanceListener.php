d013a5dc073eaa69d664f8f03bd7799888d97a15
Manon Lavenier <m.lavenier@monsieurbiz.com>
2021-09-28 10:20:30 +0200

    Add possibilty to enable or disable maintenance page

--- src/EventListener/MaintenanceListener.php
+++ src/EventListener/MaintenanceListener.php
@@ -6,12 +6,19 @@ use Symfony\Component\HttpKernel\Event\RequestEvent;
 
 class MaintenanceListener
 {
+    public $maintenance;
+
+    public function __construct($maintenance){
+        $this->maintenance=$maintenance["status"];
+    }
+
     public function onKernelRequest(RequestEvent $event){
-        $event->setResponse(
-            new Response('En maintenance',
-            Response::HTTP_SERVICE_UNAVAILABLE
-            )
-        ); 
-        $event->stopPropagation();
+        $maintenance = $this->maintenance ? $this->maintenance : false;
+        if ($maintenance === true){
+            $event->setResponse(
+                new Response('En maintenance',
+                Response::HTTP_SERVICE_UNAVAILABLE)); 
+            $event->stopPropagation();
+        }
     }
 }
