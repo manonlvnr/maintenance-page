d9c1020c24bd2a7a310d2118f1a5c7b40c3e915e
Manon Lavenier <m.lavenier@monsieurbiz.com>
2021-10-01 12:12:31 +0200

    Configure the services files for the bundle and link it to the extension file

--- lib/MaintenanceBundle/src/DependencyInjection/MbizMaintenanceExtension.php
+++ lib/MaintenanceBundle/src/DependencyInjection/MbizMaintenanceExtension.php
@@ -2,13 +2,16 @@
 
 namespace Mbiz\MaintenanceBundle\DependencyInjection;
 
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class MbizMaintenanceBundle extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     { 
-        var_dump('We\'re alive'); die;
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yaml');
     }
 }
